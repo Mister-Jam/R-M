6449775a0240ac046e5e211866211fe4956de7d9
James Tochukwu Nwankwo <james.nwankwo@kuda.com>
2024-07-09 22:26:57 +0100

    create coordinator and nav routes

--- Rick_and_Morty/Coordinators/ApplicationCoordinator.swift
+++ Rick_and_Morty/Coordinators/ApplicationCoordinator.swift
@@ -5,4 +5,43 @@
 //  Created by James Tochukwu Nwankwo on 08/07/2024.
 //
 
-import Foundation
+import UIKit
+import SwiftUI
+
+final class ApplicationCoordinator: Coordinator {
+    var parentCoordinator: Coordinator?
+    
+    var childCoordinators: [Coordinator] = []
+    let window: UIWindow
+    var rootViewController: UIViewController
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController, 
+         _ window: UIWindow) {
+        self.navigationController = navigationController
+        self.window = window
+        self.rootViewController = navigationController
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+    }
+    
+    func start() {
+        showListController()
+    }
+    
+    private func showListController() {
+        let viewModel = ListViewModel(pageTitle: "R & M Characters")
+        viewModel.didSelectItem = { [weak self] selectedCharacter in
+            self?.showDetailsScreen(characterData: selectedCharacter)
+        }
+
+        let viewController = ListViewController(viewModel: viewModel)
+        navigationController.show(viewController, sender: self)
+    }
+    
+    private func showDetailsScreen(characterData: ListItemCellModel) {
+        let view = CharacterDetailsView(characterDetails: characterData.characterDetails)
+        let hostingController = UIHostingController(rootView: view)
+        navigationController.show(hostingController, sender: self)
+    }
+}
--- Rick_and_Morty/Coordinators/Coordinator.swift
+++ Rick_and_Morty/Coordinators/Coordinator.swift
@@ -5,4 +5,32 @@
 //  Created by James Tochukwu Nwankwo on 08/07/2024.
 //
 
-import Foundation
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var parentCoordinator: Coordinator? { get set }
+    var childCoordinators: [Coordinator] { get set }
+    var rootViewController: UIViewController { get }
+    func start()
+    func addChild(_ child: Coordinator)
+    func removeChild(_ child: Coordinator)
+}
+
+extension Coordinator {
+    
+    func addChild(_ coordinator: Coordinator) {
+        guard coordinator.parentCoordinator == nil, !childCoordinators.contains(where: { $0 === coordinator }) else {
+            return
+        }
+        childCoordinators.append(coordinator)
+        coordinator.parentCoordinator = self
+    }
+    
+    func removeChild(_ coordinator: Coordinator) {
+        guard coordinator.parentCoordinator === self, childCoordinators.contains(where: { $0 === coordinator }) else {
+            return
+        }
+        childCoordinators.removeAll { $0 === coordinator }
+        coordinator.parentCoordinator = nil
+    }
+}
