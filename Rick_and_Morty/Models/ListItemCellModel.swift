6d66e411aefe2cbd3a6866fbff7fb5e2880d9af2
James Tochukwu Nwankwo <james.nwankwo@kuda.com>
2024-07-09 22:31:00 +0100

    setup list cell model and view model

--- Rick_and_Morty/Models/ListItemCellModel.swift
+++ Rick_and_Morty/Models/ListItemCellModel.swift
@@ -6,3 +6,97 @@
 //
 
 import Foundation
+
+struct ListItemCellModel {
+    let id: Int
+    let characterTitle: String
+    let characterImage: String
+    let nextPageUrl: String?
+    let characterDetails: CharacterDetailsModel
+    var nextPageAvailable: Bool {
+        return nextPageUrl.isNotNil
+    }
+    var nextPageIndex: Int? {
+        guard let url = URL(string: nextPageUrl ?? .emptyString) else { return nil }
+        let query = url.query()
+        return Int(query?.components(separatedBy: CharacterSet.decimalDigits.inverted).joined() ?? .emptyString)
+    }
+}
+
+enum RowOptions: String {
+    case created = "Created"
+    case status = "Status"
+    case species = "Species"
+    case gender = "Gender"
+    case origin = "Origin"
+    case location = "Location"
+    case episodesAppeared = "Episodes Appeared"
+}
+
+struct RowModel: Hashable {
+    let leadingText: String
+    let trailingText: String
+    
+    init(option: RowOptions, trailingText: String) {
+        self.leadingText = option.rawValue
+        self.trailingText = trailingText
+    }
+}
+
+struct CharacterDetailsModel {
+    let characterName: String
+    let characterImageString: String
+    let episodes: [String]
+    let created: String
+    let status: String
+    let species: String
+    let gender: String
+    let origin: String
+    let location: String
+    var rowModels: [RowModel] {
+        return [
+            RowModel(
+                option: .created,
+                trailingText: created),
+            
+            RowModel(
+                option: .status,
+                trailingText: status),
+            
+            RowModel(
+                option: .species,
+                trailingText: species),
+            
+            RowModel(
+                option: .gender,
+                trailingText: gender),
+            
+            RowModel(
+                option: .origin,
+                trailingText: origin),
+            
+            RowModel(
+                option: .location,
+                trailingText: location),
+            
+            RowModel(
+                option: .episodesAppeared,
+                trailingText: .emptyString)
+        ]
+    }
+    
+    var characterUrl: URL? {
+        return URL(string: characterImageString)
+    }
+    
+    static let previewVersion: CharacterDetailsModel = CharacterDetailsModel(
+        characterName: "Rick Sanchez", 
+        characterImageString: .emptyString,
+        episodes: ["1", "2", "3"],
+        created: "The past",
+        status: "Alive",
+        species: "Human",
+        gender: "Male",
+        origin: "Earth",
+        location: "Earty")
+}
--- Rick_and_Morty/ViewModel/ListViewModel.swift
+++ Rick_and_Morty/ViewModel/ListViewModel.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+
+final class ListViewModel {
+    
+    var listData: [ListItemCellModel] = []
+    let pageTitle: String
+    private let rmClient: RickAndMortyClientProvider
+    var didSelectItem: ((ListItemCellModel) -> Void)?
+    var isApiInProgress: ((Bool) -> Void)?
+    var didThrowError: ((String) -> Void)?
+    var didFetchResultData: (() -> Void)?
+    private let userInitiatedQueue = DispatchQueue(label: "com.rickandmorty.app", qos: .userInitiated)
+    private var isFetchInProgress = false
+    private var isShowingLoadingView = false
+    
+    init(pageTitle: String,
+         client: RickAndMortyClientProvider = RickAndMortyClientService.shared) {
+        self.pageTitle = pageTitle
+        self.rmClient = client
+    }
+    
+    func fetchListData(page: Int? = nil) {
+        guard !isFetchInProgress else {
+            return
+        }
+        isFetchInProgress = true
+        rmClient.fetchCharacters(
+            page: page) { [weak self] status in
+                self?.isApiInProgress?(status)
+            } completion: { [weak self] response in
+                self?.isFetchInProgress = false
+                switch response {
+                case .success(let result):
+                    self?.listData += result
+                    self?.didFetchResultData?()
+                case .failure(let error):
+                    self?.didThrowError?(error.localizedDescription)
+                }
+            }
+
+    }
+    
+    func didSelectListData(at index: Int) {
+        let selectedItem = listData[index]
+        didSelectItem?(selectedItem)
+    }
+    
+    func fetchNextPageIfPossible() {
+        guard let nextPageIndex = listData.last?.nextPageIndex else { return }
+        userInitiatedQueue.async { [weak self] in
+            self?.fetchListData(page: nextPageIndex)
+        }
+    }
+    
+    func showLoadingView() {
+        isShowingLoadingView = true
+        isApiInProgress?(isShowingLoadingView)
+    }
+    
+}
