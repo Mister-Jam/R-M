9fef1ad7289ab1b75c843cafa3ff48a4bc6df4fc
James Tochukwu Nwankwo <james.nwankwo@kuda.com>
2024-07-09 22:31:51 +0100

    create animated view

--- Rick_and_Morty/UI/LoadingView.swift
+++ Rick_and_Morty/UI/LoadingView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct LoadingView: View {
+    
+    @State private var rotation: Double = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+            ZStack {
+                Color.clear.ignoresSafeArea()
+                Circle()
+                    .fill(color)
+                    .frame(width: circleSize, height: circleSize)
+                ForEach(0..<count, id: \.self) { i in
+                    
+                    Circle()
+                        .trim(from: .zero, to: 0.1 + CGFloat(i / count))
+                        .stroke(color, style: StrokeStyle(lineWidth: 10, lineCap: .round))
+                        .frame(width: circleSize + 20 + CGFloat(i * 30), height: circleSize + 20 + CGFloat(i * 30))
+                        .rotationEffect(.degrees(rotation))
+                    
+                        .animation(Animation
+                            .linear(duration: 3.0 / Double(count - i))
+                            .repeatForever(autoreverses: false)
+                            .speed(1.3), value: rotation)
+                    
+                }
+            }
+            .onAppear() {
+                rotation = -360
+            }
+            .background(Color.blue.opacity(0.3))
+        
+        
+        
     }
+    
+    // MARK: - Drawing constants
+    let circleSize: CGFloat = 30
+    let count: Int = 6
+    let color = RadialGradient(
+        gradient: Gradient(colors: [.green, .blue]),
+        center: .center,
+        startRadius: .zero,
+        endRadius: .zero)
 }
+
 
 #Preview {
     LoadingView()
