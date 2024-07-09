7534eeaccb2aa353095b7411f6c0b67dad0180c5
James Tochukwu Nwankwo <james.nwankwo@kuda.com>
2024-07-09 22:28:54 +0100

    setup list collection view cell

--- Rick_and_Morty/UI/ListItemCollectionViewCell.swift
+++ Rick_and_Morty/UI/ListItemCollectionViewCell.swift
@@ -7,6 +7,52 @@
 
 import UIKit
 
-class ListItemCollectionViewCell: UICollectionViewCell {
+final class ListItemCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = String(describing: ListItemCollectionViewCell.self)
     
+    private lazy var imageView = UIImageView()
+    private lazy var titleLabel = UILabel(
+        font: .preferredFont(forTextStyle: .title3),
+        color: .labelDefault)
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupSubviews() {
+        contentView.addSubiews([imageView, titleLabel])
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = 12
+        imageView.layer.borderWidth = 0.3
+        imageView.layer.borderColor = UIColor(red: 205/255, green: 205/255, blue: 205/255, alpha: 1).cgColor
+        imageView.layer.masksToBounds = true
+        imageView.positionView(
+            topAnchor: contentView.topAnchor,
+            leadingAnchor: contentView.leadingAnchor,
+            trailingAnchor: contentView.trailingAnchor,
+            padding: .init(top: 10, left: 8, bottom: 10, right: 8)
+        )
+        imageView.heightAnchor.constraint(
+            equalTo: contentView.heightAnchor,
+            multiplier: 0.85
+        ).isActive = true
+        titleLabel.positionView(
+            topAnchor: imageView.bottomAnchor,
+            leadingAnchor: contentView.leadingAnchor,
+            padding: .init(top: 10, left: 8, bottom: .zero, right: 8))
+    }
+    
+    func setupCellData(with model: ListItemCellModel) {
+        imageView.setImage(with: model.characterImage, placeholderImage: .rickLogo)
+        titleLabel.text = model.characterTitle
+    }
 }
--- Rick_and_Morty/UI/ListViewController.swift
+++ Rick_and_Morty/UI/ListViewController.swift
@@ -20,6 +20,7 @@ final class ListViewController: UIViewController {
         let collectionView = UICollectionView(
             frame: .zero,
             collectionViewLayout: collectionViewLayout())
+        collectionView.backgroundColor = UIColor.sinbadBlue.withAlphaComponent(0.5)
         collectionView.showsVerticalScrollIndicator = false
         collectionView.keyboardDismissMode = .interactive
         return collectionView
@@ -31,6 +32,31 @@ final class ListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavBar()
+        setupSubviews()
+        viewModel.fetchListData()
+        bindViewModel()
+    }
+    
+    private func configureNavBar() {
+        setupNavigationBar(backgroundColor: .sinbadBlue, barTintColor: UIColor.sinbadBlue.withAlphaComponent(0.4), tintColor: .labelTitle)
+        title = viewModel.pageTitle
+    }
+    
+    private func bindViewModel() {
+        viewModel.didThrowError = { [weak self] errorMessage in
+            self?.showErrorPopup(message: errorMessage)
+        }
+        
+        viewModel.didFetchResultData = { [weak self] in
+            DispatchQueue.main.async {
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    private func setupSubviews() {
+        view.backgroundColor = .systemBackground
         addSubview(collectionView)
         collectionView.fillParentViewWithSafeAreaTop(
             padding: .init(
@@ -43,14 +69,14 @@ final class ListViewController: UIViewController {
     
     func collectionViewLayout() -> UICollectionViewCompositionalLayout {
         return UICollectionViewCompositionalLayout { [weak self] (_, _) -> NSCollectionLayoutSection? in
-                return self?.contentLayoutSection()
+            return self?.contentLayoutSection()
         }
     }
     
     private func setupCollectionView() {
         collectionView.backgroundColor = .clear
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        
+        collectionView.register(ListItemCollectionViewCell.self, forCellWithReuseIdentifier: ListItemCollectionViewCell.reuseIdentifier)
+        collectionView.prefetchDataSource = self
         collectionView.delegate = self
         collectionView.dataSource = self
     }
@@ -62,10 +88,10 @@ final class ListViewController: UIViewController {
             heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         item.contentInsets = NSDirectionalEdgeInsets(
-            top: 10,
-            leading: .zero,
+            top: .zero,
+            leading: 5,
             bottom: 10,
-            trailing: 10)
+            trailing: 5)
         
         let groupSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1),
@@ -83,16 +109,29 @@ final class ListViewController: UIViewController {
 extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return viewModel.listData.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .blue
-            return cell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListItemCollectionViewCell.reuseIdentifier, for: indexPath) as? ListItemCollectionViewCell else { return UICollectionViewCell() }
+        let model = viewModel.listData[indexPath.row]
+        cell.setupCellData(with: model)
+        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard indexPath.section != .zero else { return }
-    }    
+        viewModel.didSelectListData(at: indexPath.row)
+    }
+}
+
+extension ListViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        if shouldUpdateCollectionView(indexPaths: indexPaths) {
+            viewModel.fetchNextPageIfPossible()
+        }
+    }
+    
+    func shouldUpdateCollectionView(indexPaths: [IndexPath]) -> Bool {
+        return indexPaths.contains { $0.row >= viewModel.listData.count - 5 }
+    }
 }
