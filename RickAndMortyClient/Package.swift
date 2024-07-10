b0d51c59cb5d1e5b2719cbbbad0433f4e1745fc3
James Tochukwu Nwankwo <james.nwankwo@kuda.com>
2024-07-10 01:09:16 +0100

    reduce minimu target

--- RickAndMortyClient/Package.swift
+++ RickAndMortyClient/Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RickAndMortyClient",
+    platforms: [ .iOS(.v12), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v4)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
